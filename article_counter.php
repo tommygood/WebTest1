8b3773abcd81096c708f918514684b08737766c0
tommygood <tommy50508@gmail.com>
2022-01-07 22:07:44 +0800

    'main'

--- article_counter.php
+++ article_counter.php
@@ -1,11 +1,11 @@
 <?php
 // get the current view
-$counter = file_get_contents("http://127.0.0.1/index.html");
+$counter = file_get_contents("https://tommygood.github.io/WebTest1/index.html");
  
 if (!isset($_COOKIE['article_read'])) {
     setcookie("article_read", 1, time()+10);
     $counter++;
-    $fp = fopen("http://127.0.0.1/index.html, "w");
+    $fp = fopen("https://tommygood.github.io/WebTest1/index.html, "w");
     fwrite($fp, $counter);
     fclose($fp);
 }
