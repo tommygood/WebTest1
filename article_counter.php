eb1ed829f902023a18975c2cbf6b56978e83b118
tommygood <tommy50508@gmail.com>
2022-01-07 23:22:15 +0800

    'main'

--- article_counter.php
+++ article_counter.php
@@ -1,14 +1,14 @@
 <?php
 // get the current view
-$counter = file_get_contents("https://tommygood.github.io/WebTest1/index.html");
- 
+$counter = file_get_contents("article_counter.dat");
+
 if (!isset($_COOKIE['article_read'])) {
-    setcookie("article_read", 1, time()+10);
+    setcookie("article_read", 1, time()+3600);
     $counter++;
-    $fp = fopen("https://tommygood.github.io/WebTest1/index.html, "w");
+    $fp = fopen("article_counter.dat, "w");
     fwrite($fp, $counter);
     fclose($fp);
 }
- 
+
 echo "document.write('" . $counter . "');";
 ?>
