63b96a4eea63c61acd530731fd33195ef504861e
tommygood <tommy50508@gmail.com>
2022-01-18 00:37:00 +0800

    'main'

--- JsTest.js
+++ JsTest.js
@@ -1,2 +1,19 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+function Main(d) {
+    var v = document.getElementById('para');
+    var myHeading = document.querySelector('h1');
+    var a = "1";
+    var n = document.getElementById('aaa');
+    //alert(n.value);
+    // myHeading.textContent = n.value;
+    // myHeading.textContent = v.innerHTML;
+    console.log("a");
+}
+function ChangeWeb() {
+    var myHeading = document.querySelector('h1');
+    var n = document.getElementById('aaa');
+    if (n.value == "王冠權") {
+        window.location.href='https://tommygood.github.io/WebTest1/index.html';
+    }
+    else
+        myHeading.textContent = "這誰";
+}
